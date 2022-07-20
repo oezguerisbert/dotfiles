763de3b5d4ca71d6424cb811663dda9172f00a83
Özgür Isbert <oezguerisbert@gmail.com>
2022-07-20 09:52:07 +0200

    fix: reverted from prismaFmt, could find the binary for it

--- .config/nvim/lua/oezguerisbert.lua
+++ .config/nvim/lua/oezguerisbert.lua
@@ -41,7 +41,6 @@ null_ls.setup({
 	null_ls.builtins.diagnostics.pylint,
         null_ls.builtins.code_actions.eslint,
         null_ls.builtins.formatting.prettierd,
-	null_ls.builtins.formatting.prismaFmt,
 	null_ls.builtins.formatting.djhtml,
 	null_ls.builtins.formatting.djlint,
 	null_ls.builtins.formatting.isort,
