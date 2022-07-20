d971ddd3d040d8fe0555cce0825ce8c9455563c7
Özgür Isbert <oezguerisbert@gmail.com>
2022-07-20 09:35:50 +0200

    feat: more and maybe better formatter/diagnostics for languages I use

--- .config/nvim/lua/oezguerisbert.lua
+++ .config/nvim/lua/oezguerisbert.lua
@@ -38,8 +38,13 @@ require'lspconfig'.tsserver.setup{
 null_ls.setup({
     sources = {
         null_ls.builtins.diagnostics.eslint,
+	null_ls.builtins.diagnostics.pylint,
         null_ls.builtins.code_actions.eslint,
-        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.prettierd,
+	null_ls.builtins.formatting.prismaFmt,
+	null_ls.builtins.formatting.djhtml,
+	null_ls.builtins.formatting.djlint,
+	null_ls.builtins.formatting.isort,
     },
     on_attach = on_attach,
 })
