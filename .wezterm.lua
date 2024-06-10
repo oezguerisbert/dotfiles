4323e64b9b7875e55a915d4bf8226ec4ec0ea325
Özgür Isbert <oezguerisbert@gmail.com>
2024-06-10 09:30:14 +0200

    chores: turned off ligatures

--- .wezterm.lua
+++ .wezterm.lua
@@ -19,6 +19,8 @@ config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
 
+config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+
 config.colors = {
   tab_bar = {
     -- The color of the strip that goes along the top of the window
