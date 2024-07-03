e9413dede86f0398ce7dfe2a0709ee0259309c9a
Özgür Isbert <oezguerisbert@gmail.com>
2024-07-03 21:14:45 +0200

    change of background

--- .wezterm.lua
+++ .wezterm.lua
@@ -25,14 +25,14 @@ config.colors = {
   tab_bar = {
     -- The color of the strip that goes along the top of the window
     -- (does not apply when fancy tab bar is in use)
-    background = '#000000',
+    background = '#080808',
 
     -- The active tab is the one that has focus in the window
     active_tab = {
       -- The color of the background area for the tab
       bg_color = '#ffffff',
       -- The color of the text for the tab
-      fg_color = '#000000',
+      fg_color = '#080808',
 
       -- Specify whether you want "Half", "Normal" or "Bold" intensity for the
       -- label shown for this tab.
@@ -55,7 +55,7 @@ config.colors = {
 
     -- Inactive tabs are the tabs that do not have focus
     inactive_tab = {
-      bg_color = '#000000',
+      bg_color = '#080808',
       fg_color = '#dddddd',
 
       -- The same options that were listed under the `active_tab` section above
@@ -75,7 +75,7 @@ config.colors = {
 
     -- The new tab button that let you create new tabs
     new_tab = {
-      bg_color = '#000000',
+      bg_color = '#080808',
       fg_color = '#eeeeee',
 
       -- The same options that were listed under the `active_tab` section above
